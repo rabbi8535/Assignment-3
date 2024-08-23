86852d1b4c3c0b0f6f8f60e3c06ecb038a3f822b
rabbi8535 <rabbi8535@gmail.com>
2024-08-23 22:51:20 +0600

    added header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                clifford: '#da373d',
+               'txt':'rgb(33, 33, 33)',
+               'txt_2':'rgb(73, 73, 73)',
+                'arrow': 'rgb(171, 239, 95)',
+                'arrow_2':'rgba(171, 239, 95, 0.5)',
+                'bars' : 'rgb(115, 115, 115)',
+                'black_ar' : 'rgb(28, 28, 28)',
+                'header_1': 'rgba(245, 245, 245)',
+                'header_2': 'rgba(255, 255, 255,0)',
+
             },
+            /* custom font */
             fontFamily: {
                 'manrope': ["Manrope", "sans-serif"]
             }
